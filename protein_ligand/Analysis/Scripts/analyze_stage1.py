32e62cb5251542c9aeaad4e76109f50ee6ea0350
MehtapIsik <mehtap.isik@choderalab.org>
2020-01-30 14:00:15 -0500

    Create complete collection file with missing non-binder predictions, and update analysis.

--- protein_ligand/Analysis/Scripts/analyze_stage1.py
+++ protein_ligand/Analysis/Scripts/analyze_stage1.py
@@ -30,6 +30,7 @@ from pkganalysis.stats import (compute_bootstrap_statistics, calc_confusion_matr
 STAGE_1_SUBMISSIONS_DIR_PATH = '../Submissions-stage1/'
 EXPERIMENTAL_DATA_FILE_PATH = '../../experimental-data/stage-1/hits_verification.csv'
 USER_MAP_FILE_PATH = '../Analysis-outputs-stage1/SAMPL7-user-map-PL-stage1.csv'
+FRAGMENTS_FILE_PATH = '../../fragments_screened.csv'
 
 # =============================================================================
 # MAIN CHALLENGE SUBMISSION 1
@@ -223,6 +224,109 @@ class Stage1SubmissionCollection:
             self.data.to_csv(stage1_submission_collection_file_path, index=False)
 
             print("Stage1 submission collection file generated:\n", stage1_submission_collection_file_path)
+
+
+    def complete_predictions_with_missing_fragments(self, fragments_file_path):
+        """ Adds missing non-binder predictions to collection. Assumes
+        Parameters
+        ----------
+        fragments_file_path: Path to CSV file with fragment IDs in the first column
+
+        """
+
+        # Read fragments file to extract full list of screened fragment list IDs
+        fragments_data = pd.read_csv(fragments_file_path, names=["Fragment ID", "SMILES"])
+        fragment_IDs = fragments_data["Fragment ID"].values
+
+        # Rebuild full stage 1 collection table, adding missing fragments (predicted as non-binder).
+        data = []
+
+        # Submissions for stage 1.
+        for submission in submissions:
+            if submission.reference_submission and ignore_refcalcs:
+                continue
+
+            # print("submission.sid:\n", submission.sid)
+            # print("submission.name:\n", submission.name)
+            # print("submission.data:\n", submission.data)
+
+            for screened_fragment_ID in fragment_IDs:
+                #screened_fragment_ID = "F560"
+                #print("screened_fragment_ID:", screened_fragment_ID)
+
+                # Check if screened fragment is in submission
+                submitted_fragment_IDs = set(submission.data.index.values)
+               # print("submitted_fragment_IDs:\n", submitted_fragment_IDs)
+
+                # If screened fragment ID is already in the submission set, take prediction records from the submission
+                if screened_fragment_ID in submitted_fragment_IDs:
+                    #print("Already submitted.")
+
+                    series = submission.data.loc[screened_fragment_ID,:]
+                    #print("series:\n", series)
+                    #site1_pred = series["Site 1"]
+                    #print("site1_pred: ", site1_pred)
+
+                    # Predicted data
+                    site1_pred = series["Site 1"]
+                    site2_pred = series["Site 2"]
+                    site3_pred = series["Site 3"]
+                    site4_pred = series["Site 4"]
+                    all_sites_pred = series["All Sites"]
+
+                # If screened fragment ID was not in the submitted prediction set, add a non-binder prediction to data
+                else:
+                    #print("Not submitted.")
+
+                    # Predicted data
+                    site1_pred = "False"
+                    site2_pred = "False"
+                    site3_pred = "False"
+                    site4_pred = "False"
+                    all_sites_pred = "False"
+
+                # Experimental data
+                site1_exp = experimental_data.loc[screened_fragment_ID, 'Site 1']
+                site2_exp = experimental_data.loc[screened_fragment_ID, 'Site 2']
+                site3_exp = experimental_data.loc[screened_fragment_ID, 'Site 3']
+                site4_exp = experimental_data.loc[screened_fragment_ID, 'Site 4']
+                all_sites_exp = experimental_data.loc[screened_fragment_ID, 'All Sites']
+
+                data.append({
+                   'SID': submission.sid,  # Previously receipt_ID
+                    'Participant': submission.participant,
+                    'Organization': submission.organization,
+                    'Name': submission.name,
+                    'Category': submission.category,
+                    'Ranked': submission.ranked,
+                    'Fragment ID': screened_fragment_ID,
+                    'Site 1 (pred)': site1_pred,
+                    'Site 1 (exp)': site1_exp,
+                    'Site 2 (pred)': site2_pred,
+                    'Site 2 (exp)': site2_exp,
+                    'Site 3 (pred)': site3_pred,
+                    'Site 3 (exp)': site3_exp,
+                    'Site 4 (pred)': site4_pred,
+                    'Site 4 (exp)': site4_exp,
+                    'All Sites (pred)': all_sites_pred,
+                    'All Sites (exp)': all_sites_exp
+                    })
+
+            #print("data:\n", data)
+
+            # Transform into Pandas DataFrame.
+            self.data = pd.DataFrame(data=data)
+
+            print("\n SubmissionCollection: \n")
+            print(self.data)
+
+            # Create general output directory.
+            os.makedirs(self.output_directory_path, exist_ok=True)
+
+            # Save completed collection.data dataframe in a CSV file.
+            self.data.to_csv(stage1_submission_collection_file_path, index=False)
+            print("Stage1 submission collection file updated with missing predictions:\n", stage1_submission_collection_file_path)
+
 
 
     # TO DO: The following function does not pertain to this challenge/needs updating if we even retain
@@ -453,13 +557,11 @@ class Stage1SubmissionCollection:
 
 if __name__ == '__main__':
 
-
     # Read experimental data.
     with open(EXPERIMENTAL_DATA_FILE_PATH, 'r') as f:
         #names = ('Fragment ID', 'Site 1', 'Site 2', 'Site 3', 'Site 4', 'All Sites')
         experimental_data = pd.read_csv(f, sep=',', index_col='Fragment ID', header=0)
         print("Experimental data of stage 1:\n",experimental_data)
-
 
 
     #Import user map.
@@ -510,6 +612,7 @@ if __name__ == '__main__':
         'False Positive': 'False Positive',
     }
 
+
     # Load submissions data.
     print("Loading submissions...")
     submissions = load_submissions(Stage1Submission, STAGE_1_SUBMISSIONS_DIR_PATH, user_map)
@@ -528,7 +631,8 @@ if __name__ == '__main__':
                                             stage1_submission_collection_file_path, ignore_refcalcs = False)
 
     # Supplement submission collection with missing fragments predicted as non-binders
-    # TO-DO
+    collection.complete_predictions_with_missing_fragments(fragments_file_path=FRAGMENTS_FILE_PATH)
+
 
     # Generate statistics tables for all submissions
     sns.set_context('talk')
@@ -539,6 +643,8 @@ if __name__ == '__main__':
                                           latex_header_conversions=latex_header_conversions,
                                           caption=caption, ignore_refcalcs=False)
 
+
     # Generate statistics tables for only ranked submissions
     # TO-DO
+    #OUTPUT_DIRECTORY_PATH = '../Analysis-outputs-stage1-ranked-only'
 
